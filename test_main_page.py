939cf30534db5e7f4a53548fe0a897f8a89d7b12
Alex <practicum.alex@yandex.ru>
2023-05-30 17:42:32 +0300

    Cleanup

--- test_main_page.py
+++ test_main_page.py
@@ -4,46 +4,47 @@ from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
     
-#def test_guest_should_see_login_link(browser):
-#    link = "http://selenium1py.pythonanywhere.com/"
-#    page = MainPage(browser, link)
-#    page.open()
-#    page.should_be_login_link()
-#    
-#def test_guest_can_go_to_login_page(browser):
-#    link = "http://selenium1py.pythonanywhere.com/"
-#    main_page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-#    main_page.open()                      # открываем страницу
-#    main_page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-#    login_page = LoginPage(browser, browser.current_url)
-#    login_page.should_be_login_page()
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                     # открываем страницу
+    page.should_be_login_link()     # проверяем наличие ссылки на страницу логина
+    
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    main_page = MainPage(browser, link)   
+    main_page.open()                      
+    main_page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
-#@pytest.mark.parametrize('link0', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-#                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",marks=pytest.mark.xfail),
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-#def test_guest_can_add_product_to_basket(browser, link0):
-#    link1 = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-#    link2 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-#    link = link0
-#    page = ProductPage(browser, link)
-#    page.open()
-#    product_attributes = page.get_product_attributes()
-#    page.add_product_to_basket()
-#    basket_page = BasketPage(browser, browser.current_url)
-#    basket_product_attributes = basket_page.get_products_attributes()
-#    assert product_attributes == basket_product_attributes, '\nAttributes of product in basket does not match item added!'
+@pytest.mark.skip(reason="no need of currently testing this")
+@pytest.mark.parametrize('link0', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link0):
+    link1 = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link2 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    link = link0
+    page = ProductPage(browser, link)
+    page.open()
+    product_attributes = page.get_product_attributes()
+    page.add_product_to_basket()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_product_attributes = basket_page.get_products_attributes()
+    assert product_attributes == basket_product_attributes, '\nAttributes of product in basket does not match item added!'
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-    page = ProductPage(browser, link, 0)
+    page = ProductPage(browser, link, 0) # 0 - disable implicitly_wait
     page.open()
     page.add_product_to_basket(check_details = False)
     page.should_not_be_success_message()
